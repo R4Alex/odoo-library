52e8b8274aeb0d9d4eff53ef5c81bfdb91ee7074
Alejandro Santillan <asantillan@modoole.com>
2020-08-14 20:04:39 -0600

    [ADD] fields

--- library/models/rental.py
+++ library/models/rental.py
@@ -11,3 +11,10 @@ class Rentals(models.Model):
 
     rental_date = fields.Date()
     return_date = fields.Date()
+
+    customer_address = fields.Text(related='customer_id.address')
+    customer_email = fields.Char(related='customer_id.email')
+
+    book_authors = fields.Many2many(related='book_id.author_ids')
+    book_edition_date = fields.Date(related='book_id.edition_date')
+    book_publisher = fields.Many2one(related='book_id.publisher_id')
